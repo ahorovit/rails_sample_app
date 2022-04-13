1bf18fa3fcb457196192b1c3c79f7a047585ed4e
Adin Horovitz <adin@mightynetworks.com>
2022-04-13 13:23:30 -0700

    add password/authentication

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   test "should be valid" do
@@ -53,5 +54,15 @@ class UserTest < ActiveSupport::TestCase
     @user.save
 
     assert_not duplicate.valid?
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length of 6" do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 end
